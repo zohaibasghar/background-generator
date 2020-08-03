3f4ff6c789e2d6e361da03438b7ce7044ee73125
zohaib Asghar <32808057+zohaibasghar@users.noreply.github.com>
2020-08-03 22:33:38 +0500

    c

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('this site is synced with git');
+var css=document.querySelector('h3');
+var clr1=document.querySelector('.color1');
+var clr2=document.querySelector('.color2');
+var body=document.querySelector('body');
+
+function set(){
+    body.style.background=
+    'linear-gradient(to right(,'+clr1.value+','+clr2.value+')';
+    css.textContent=body.style.background
+}
+clr1.addEventListener('input',set());
+clr2.addEventListener('input',set());
